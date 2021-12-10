e70e616adb5796a1ba715d25915e5372c9de7cc9
Developer-Nasim <ajnasim72@gmail.coom>
2021-12-10 06:00:41 -0800

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,7 @@
   $('.siteBar-btn').click( function (){ 
     $('.mobile-menu').toggleClass('siteBar');   
   }); 
-
-  
-
+ 
 
   // owlCarousel
   $(".brand-active").owlCarousel({
@@ -33,28 +31,50 @@
     }
   });
 
+
+
   if (document.querySelector('select')) {
    
     $('select').niceSelect(); 
 
   }
+
+
+  // Dasboard mobile menu
+  function DasboardMobileMenu() {
+    let MenuBlk = document.querySelector('.dasboard-sidebar-menu')
+    let allBtn = document.querySelectorAll('.dmBtn');
+    allBtn.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        MenuBlk.classList.toggle('active')
+      })
+    });
+  }
+  DasboardMobileMenu()
+
  
 
   // accordion
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-  
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
-  }
+  function Accordion() {
+    
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
 
- 
+  }
+  Accordion()
+
+
+
 })(jQuery);
